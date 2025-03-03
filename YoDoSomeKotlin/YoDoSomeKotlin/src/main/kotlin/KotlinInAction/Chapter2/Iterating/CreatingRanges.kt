4f5ab8b585a469b0bc1425742fb3196e50407ed8
Vaibhav Pathak <vav8085@gmail.com>
2025-03-03 11:59:08 -0500

    better use of ranges

---
+++ YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter2/Iterating/CreatingRanges.kt
@@ -0,0 +1,11 @@
+package com.vav.KotlinInAction.Chapter2.Iterating
+
+fun main(){
+    println(isNotOddLessThan10(3))
+}
+
+//We can check if c exist in a range using "in" operator.
+fun isLetter(ch: Char) = ch in 'A'..'Z' || ch in 'a'..'z'
+
+//we can also use step and !in if we dont want
+fun isNotOddLessThan10(i: Int) = i !in 1..11 step 2
