43d44625ef92eb894d86d4e5a87d8e26afddb84b
Vaibhav Pathak <vav8085@gmail.com>
2025-03-03 03:24:57 -0500

    conditional statements

--- YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter2/SmartCast/Main.kt
+++ YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter2/SmartCast/Main.kt
@@ -13,6 +13,8 @@ fun main(){
     In Kotlin compiler already knows the value of type so we can skip "val num =  e as Num"
 
     Notice that in the else if statement we are not checking the type
+
+    Also notice that there is a background color showing for e. This means this value is a smartcast
  */
 fun eval(e: Expr): Int{
     if(e is Num){
@@ -22,5 +24,52 @@ fun eval(e: Expr): Int{
         return eval(e.left) + eval(e.right)
     }else if (e is Product){
         return eval(e.left) * eval(e.right)
+    } else throw IllegalArgumentException("Incorrect expression!")
+}
+
+//a better eval using property of if expression
+
+fun eval1(e: Expr): Int{
+    return if(e is Num){
+        e.value
+    }else if(e is Sum){
+        eval(e.left) + eval(e.right)
+    }else if (e is Product){
+        eval(e.left) * eval(e.right)
+    } else throw IllegalArgumentException("Incorrect expression!")
+}
+
+//more concise
+
+fun eval2(e: Expr): Int{
+    return if(e is Num) e.value
+    else if(e is Sum) eval(e.left) + eval(e.right)
+    else if (e is Product) eval(e.left) * eval(e.right)
+    else throw IllegalArgumentException("Incorrect expression!")
+}
+
+//using when. Notice that in this when we check is Num ->
+//this is a special type of when where we can check the type of the argument
+fun eval3(e: Expr): Int{
+    return when (e) {
+        is Num -> e.value
+        is Sum -> eval(e.left) + eval(e.right)
+        is Product -> eval(e.left) * eval(e.right)
+        else -> throw IllegalArgumentException("Incorrect expression!")
+    }
+}
+
+//the last statement in a block is the value that is returned
+//this does not hold true for regular functions. regular functions have to have a return statement
+fun eval4(e: Expr): Int{
+    return if(e is Num){
+        println(e.value)
+        e.value
+    }else if(e is Sum){
+        println("${e.left}...${e.right}")
+        eval(e.left) + eval(e.right)
+    }else if (e is Product){
+        println("${e.left}...${e.right}")
+        eval(e.left) * eval(e.right)
     } else throw IllegalArgumentException("Incorrect expression!")
 }
--- YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter2/SmartCast/Product.kt
+++ YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter2/SmartCast/Product.kt
@@ -1,3 +1,3 @@
 package com.vav.KotlinInAction.Chapter2.SmartCast
 
-class Product(val left: Expr, val right: Expr): Expr
+class Product(var left: Expr, val right: Expr): Expr
