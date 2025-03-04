5c3b8e90b182d0fa9718766d36c6f21795c40d31
Vaibhav Pathak <vav8085@gmail.com>
2025-03-03 23:01:53 -0500

    better use of ranges

--- YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter2/Iterating/CreatingRanges.kt
+++ YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter2/Iterating/CreatingRanges.kt
@@ -9,3 +9,9 @@ fun isLetter(ch: Char) = ch in 'A'..'Z' || ch in 'a'..'z'
 
 //we can also use step and !in if we dont want
 fun isNotOddLessThan10(i: Int) = i !in 1..11 step 2
+
+fun checkVarious(c: Char) = when(c){
+    in '1'..'9' -> "integer"
+    in 'A'..'Z' -> "character"
+    else -> "In don't know"
+}
