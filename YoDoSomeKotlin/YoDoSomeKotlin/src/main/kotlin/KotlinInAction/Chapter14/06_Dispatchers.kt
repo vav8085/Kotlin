bf7129afad0ed8c181f1b7d756998ee47950c5d5
Vaibhav Pathak <vav8085@gmail.com>
2025-05-17 11:14:10 -0400

    dispatchers

--- YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter14/06_Dispatchers.kt
+++ YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter14/06_Dispatchers.kt
@@ -1,2 +1,11 @@
 package com.vav.KotlinInAction.Chapter14
 
+/*
+    *   Dispatchers determine where a coroutine will run.
+    *   They dispatch a coroutine to a thread.
+    *   It can run on background thread.
+    *   It can also run on a thread pool.
+    *   Coroutines are usually not bound to a specific thread.
+    *   There come dispatchers into the picture.
+    *   A coroutine can suspend in one thread and resume in another.
+ */
