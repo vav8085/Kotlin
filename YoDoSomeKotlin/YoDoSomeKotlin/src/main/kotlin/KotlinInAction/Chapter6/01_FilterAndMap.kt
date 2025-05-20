f74155a38c2a89343e1fb6495ac800831f881052
Vaibhav Pathak <vav8085@gmail.com>
2025-05-20 07:06:46 -0400

    filter and map

---
+++ YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter6/01_FilterAndMap.kt
@@ -0,0 +1,60 @@
+package com.vav.KotlinInAction.Chapter6
+
+
+/*
+    *   Filter function is used to filter some collection and get the values that can pass through the filter condition.
+    *   Filter does not transform the collection it only extracts the values from the collection.
+
+    *   Map allows you to transform the collection and return the output of transformation.
+ */
+
+
+data class Person(val name: String, val age: Int)
+
+fun main(){
+    filterEx()
+    separator()
+    filterPeople()
+    separator()
+    mapEx()
+    separator()
+    mapPeople()
+    separator()
+    filterMapChain()
+}
+
+fun separator(){
+    println("-----------------------------------------------------------")
+}
+
+fun filterEx(){
+    val list = listOf(1,2,3,4,5)
+    println(list.filter { it > 3 })
+}
+//[4, 5]
+
+fun filterPeople(){
+    val people = listOf(Person("John", 30), Person("Jason", 25), Person("Bond", 35))
+    println(people.filter { it.age > 30 }.toString())
+}
+//[Person(name=Bond, age=35)]
+
+fun mapEx(){
+    val list = listOf(1,2,3,4,5,6,7)
+    println(list.map { it * it })
+}
+//[1, 4, 9, 16, 25, 36, 49]
+
+fun mapPeople(){
+    val people = listOf(Person("John", 30), Person("Jason", 25), Person("Bond", 35))
+    println(people.map { Pair(it.name, it.age) })
+    println(people.map { it.name })
+}
+//[(John, 30), (Jason, 25), (Bond, 35)]
+//[John, Jason, Bond]
+
+fun filterMapChain(){
+    val people = listOf(Person("John", 30), Person("Jason", 25), Person("Bond", 35))
+    println(people.filter { it.age > 30 }.map { it.name })
+}
+//[Bond]
