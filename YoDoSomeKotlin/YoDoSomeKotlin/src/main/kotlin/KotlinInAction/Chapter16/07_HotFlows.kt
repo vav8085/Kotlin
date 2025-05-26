e23ed9f44ae22f4e11a93264c32a76fb004536f6
Vaibhav Pathak <vav8085@gmail.com>
2025-05-26 17:49:43 -0400

    hot flows start

---
+++ YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter16/07_HotFlows.kt
@@ -0,0 +1,12 @@
+package com.vav.KotlinInAction.Chapter16
+
+/*
+    *   Hot Flows share emitted items across multiple subscribers
+    *   They are always active and unlike cold flows, collectors dont trigger them.
+    *   2 Types:
+            1.  Shared Flows - used for broadcasting
+            2.  State Flows - special case of communicating state
+
+    *   
+
+ */
