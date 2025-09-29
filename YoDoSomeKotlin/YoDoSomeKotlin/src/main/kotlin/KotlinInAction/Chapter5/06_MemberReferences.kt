64aad932b8f6b630798f0d2a0417ebeda864446b
Vaibhav Pathak <vav8085@gmail.com>
2025-09-28 17:32:33 -0700

    bounded member references

--- YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter5/06_MemberReferences.kt
+++ YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter5/06_MemberReferences.kt
@@ -8,9 +8,12 @@ import com.vav.KotlinInAction.Chapter6.personAge
     Lambda allows us to pass a code as a function.
     But if the code itself is a function and nothing more?
 
+    Then lambda can be replaced by something called member references.
+
     In below example the function maxBy accepts a lambda that takes input as a Person
     and returns an age. It does nothing more
     So we separate Person and age with a :: which is called member referencing.
+    Person is the first parameter of lambda and then comes :: and member.
 
     Member references are of 2 types:
     1. Unbound member references
@@ -20,11 +23,13 @@ import com.vav.KotlinInAction.Chapter6.personAge
     but it must have lambda as a param.
     The lambda itself can have many params but the first param should be of the same type as the
     class of the member reference.
-    The member reference if its a method must match the remaining params of the lambda in signature
+    The member reference if it's a method must match the remaining params of the lambda in signature
 
     for example in below code:
     * the function fun stop() of the Vehicle matches the stop lambda params which is just class
     * The function fun drive(speed: Int) matches the drive lambda with params which is Vehicle, Int
+
+    Receiver: The first parameter that is the object of the class itself is called Receiver.
 
     The reason these are called unbound member references is because they are not bound to an object yet
  */
--- YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter5/07_BoundMemberReferences.kt
+++ YoDoSomeKotlin/YoDoSomeKotlin/src/main/kotlin/KotlinInAction/Chapter5/07_BoundMemberReferences.kt
@@ -1,2 +1,28 @@
 package com.vav.KotlinInAction.Chapter5
 
+/*
+
+    Bound member references are bound to an object
+
+    You can use bounded member references to replace a lambda that does not take any parameter
+
+    usually you will call the method using . compute1 { operation.add() }
+
+    it just converts to compute1 (operation::add)
+ */
+
+fun main(){
+    val operation = Operation()
+    compute1 { operation.add() }
+    compute1 (operation::add)
+}
+
+fun compute1(lambda: () -> Unit){
+
+}
+
+class Operation{
+    fun add(){
+
+    }
+}
