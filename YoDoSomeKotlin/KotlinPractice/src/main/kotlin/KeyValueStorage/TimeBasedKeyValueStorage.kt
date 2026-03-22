0c9e95fe6ec2aa589d727e720295e5218cbf9a4d
Vaibhav Pathak <vav8085@gmail.com>
2026-03-21 19:27:05 -0700

    Time based key value storage

---
+++ YoDoSomeKotlin/KotlinPractice/src/main/kotlin/KeyValueStorage/TimeBasedKeyValueStorage.kt
@@ -0,0 +1,51 @@
+package org.example.KeyValueStorage
+
+import java.util.TreeMap
+
+/*
+    Very similar to time based key value store leetcode problem
+
+    Treemap has a floorKey method that can give the floor timestamp of requested timestamp.
+    this can help retrieving the floor value if any.
+
+    The other solution can be linear / binary searching the floor value.
+ */
+class TimeBasedKeyValueStorage {
+    // key -> (field -> (timestamp -> value))
+    val cache = hashMapOf<String, HashMap<String, TreeMap<Int, String?>>>()
+
+    fun setAt(key: String, field: String, value: String, timestamp: Int) {
+        cache.getOrPut(key) { hashMapOf() }.getOrPut(field){ TreeMap() }[timestamp] = value
+
+//        val record = cache.getOrDefault(key, hashMapOf())
+//        val history = record.getOrDefault(field, TreeMap())
+//        history.put(timestamp, value)
+//        record.put(field, history)
+//        cache.put(key, record)
+    }
+
+    fun getAt(key: String, field: String, timestamp: Int): String? {
+        val history = cache[key]?.get(field) ?: return null
+        val floorTime = history.floorKey(timestamp) ?: return null
+        return history.get(floorTime)
+    }
+
+    fun deleteAt(key: String, field: String, timestamp: Int): Boolean {
+        val record = cache[key] ?: return false
+        val history = record[field] ?: return false
+        val floorTime = history.floorKey(timestamp) ?: return false
+        if(history[floorTime] != null) {
+            history[timestamp] = null
+            return true
+        } else return false
+    }
+
+    fun scanAt(key: String, timestamp: Int): List<String> {
+        
+    }
+
+    fun scanByPrefixAt(key: String, prefix: String, timestamp: Int): List<String> {
+
+    }
+
+}
