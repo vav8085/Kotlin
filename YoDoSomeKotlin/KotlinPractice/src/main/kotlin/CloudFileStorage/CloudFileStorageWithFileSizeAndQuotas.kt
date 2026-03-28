78b4089eb22054d0e187f0148cd73b052490406d
Vaibhav Pathak <vav8085@gmail.com>
2026-03-27 20:23:49 -0700

    upload file

--- YoDoSomeKotlin/KotlinPractice/src/main/kotlin/CloudFileStorage/CloudFileStorageWithFileSizeAndQuotas.kt
+++ YoDoSomeKotlin/KotlinPractice/src/main/kotlin/CloudFileStorage/CloudFileStorageWithFileSizeAndQuotas.kt
@@ -1,5 +1,54 @@
 package org.example.CloudFileStorage
 
 class CloudFileStorageWithFileSizeAndQuotas {
-    
+
+    //user -> (filename -> size)
+    val storage = hashMapOf<String, HashMap<String, Int>>()
+
+    //user -> capacity
+    val quota = hashMapOf<String, Int>()
+
+    fun addUser(userId: String, capacity: Int): Boolean {
+        if (userId in quota) return false
+        storage[userId] = hashMapOf()
+        quota[userId] = capacity
+        return true
+    }
+
+    fun uploadFile(userId: String, fileName: String, size: Int): String {
+        if(!storage.contains(userId) || !quota.contains(userId)) return "failed"
+
+        val record = storage.getOrDefault(userId, hashMapOf())
+
+        var totalSize = 0
+
+        for(key in record.keys) totalSize += record[key] ?: 0
+
+
+        val capacity = quota[userId] ?: 0
+
+        if(record.contains(fileName)){
+            val existingFileSize = record.getOrDefault(fileName, 0)
+            if(totalSize - existingFileSize + size <= capacity){
+                record[fileName] = size
+                storage[userId] = record
+                return "overwritten"
+            }else{
+                return "failed"
+            }
+        }else{
+            if(size + totalSize <= capacity) {
+                record[fileName] = size
+                storage[userId] = record
+                return "uploaded"
+            } else return "failed"
+        }
+    }
+
+    fun getFile(userId: String, fileName: String): String? {
+    }
+
+    fun deleteFile(userId: String, fileName: String): Boolean {
+
+    }
 }
